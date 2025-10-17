aac01ab0671f44fc0e54a828fd5859e954811140
Maxwell-Batt <a1769961@student.adelaide.edu.au>
2025-10-17 07:04:10 +0000

    basic input without using enter

---
+++ basicInput.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <ncurses.h>
+
+int main() {
+    initscr(); // Initialize ncurses
+    cbreak();  // Disable line buffering
+    noecho();  // Disable echoing
+    
+    char key = 'h';
+    while(key != 'q'){
+        printw("Press a key (without Enter): ");
+        key = getch(); // Read a single character
+        printw("\nYou pressed: %c\n", key);
+        refresh(); // Update the screen
+    }
+    endwin(); // End curses mode
+    return 0;
+}
